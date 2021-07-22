151be49ad78a6f43ffbf275a8f6a0b657cbc184b
gui <guinog2002@live.com.pt>
2021-07-22 11:43:57 +0100

    Login page
    Criar o formulário de login que liga à home page com botão de criar evento

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import "../styles/Login.css"
+import { useHistory } from "react-router";
+function Login() {
+    const history = useHistory();
+    return (
+        <div>
 
-function Login(){
-    return(
-        <h1><button>Login</button></h1>
+            <h1>Login</h1>
+
+            <div>
+
+                <form>
+                    <label className="emaillabel" >E-mail</label>
+                    <input
+                        type="email"
+                        placeholder="Insira o seu e-mail"
+                        className="emailinput"
+                        required
+                    />
+
+                    <label className="passwordlabel" >Password</label>
+                    <input
+                        type="password"
+                        placeholder="Insira a sua password"
+                        className="passwordinput"
+                        required
+                    />
+
+                    <div>
+                        <button type="submit" className="signinbutton" onClick={() => history.push("/homepage")}
+                        >Efetuar Login
+                        </button>
+
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div>
     )
 }
 

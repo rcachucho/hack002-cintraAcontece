07aa663dff9fae67bd938bb7b830254606cd6632
rcachucho <rodrigo_cachucho@hotmail.com>
2021-07-22 12:19:16 +0100

    set timeOut a funcionar

--- frontend/src/components/Event.jsx
+++ frontend/src/components/Event.jsx
@@ -4,27 +4,27 @@ function Event() {
     const [events, setEvents] = useState([])
     const [eventDisplay, setEventDisplay] = useState({})
     const [userClick, setUserClick] = useState(false)
+    const [iid, setIid] = useState(false)
 
     // As the page loads, loads the event, changes a random event time-based and gets from BE a random event
     useEffect(() => {
         fetchEvents()
     }, [])
 
-
     // It sets an interval in wich the randomEvent displayed at the bottom of the page will change. When the user clicks, it stops
     function changeEventOnTime() {
-        if (!userClick) {
-            setInterval(() => {
+        setIid(setInterval(() => {
+            if (!userClick) {
                 fetchRandomEvent()
-            }, 7500)
-        }
+            }
+        }, 7500))
     }
 
     // As the user clicks on an event, it changes the event displayed at the bottom to that specific event. It also stops the changeEventOnTime
-    function handleClick(id) {
-        fetchEventById(id);   
-        setUserClick(true);    
-        clearInterval()
+    const handleClick = (id) => {
+        fetchEventById(id);
+        clearInterval(iid)
+        setUserClick(true);
     }
 
     // POST - It sends the eventId to show that specific event
@@ -53,7 +53,6 @@ function Event() {
 
     // Loads all the events, already sorted by date
     async function fetchEvents() {
-        console.log("A correr fetch events")
         const res = await fetch("/events")
         const resBody = await res.json()
         if (res.status === 200) {
